c5ce2da4ad888f4f660941c3d138b70c972934be
Brynner Jay Antiquesa <imbamystery@gmail.com>
2026-06-16 13:37:40 +0800

    add functionality of the number buttons to register to the number buffer and display numbers on the screen

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let firstOperand;
 let secondOperand;
 let operator;
+let total;
+let numBuffer = ``;
+
+const numButtons = document.querySelector('#num-buttons');
+const opButtons = document.querySelector('#op-buttons');
+const screenText = document.querySelector('#screen-text');
 
 function add(firstOp, secondOp) {
     let result = firstOp + secondOp;
@@ -46,3 +52,54 @@ function operate(fOP, sOP, op) {
             break;
     }
 }
+
+function updateNum(newStr) {
+    numBuffer = `${numBuffer}${newStr}`;
+    screenText.textContent = numBuffer;
+}
+
+numButtons.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch (target.id) {
+        case 'seven':
+            updateNum(7);
+            break;
+        case 'eight':
+            updateNum(8);
+            break;
+        case 'nine':
+            updateNum(9);
+            break;
+        case 'four':
+            updateNum(4);
+            break;
+        case 'five':
+            updateNum(5);
+            break;
+        case 'six':
+            updateNum(6);
+            break;
+        case 'one':
+            updateNum(1);
+            break;
+        case 'two':
+            updateNum(2);
+            break;
+        case 'three':
+            updateNum(3);
+            break;
+        case 'zero':
+            updateNum(0);
+            break;
+        case 'dot':
+            updateNum('.');
+            break;
+        case 'clear':
+            numBuffer = ``;
+            screenText.textContent = ``;
+            break;
+        default:
+            break;
+    }
+});
